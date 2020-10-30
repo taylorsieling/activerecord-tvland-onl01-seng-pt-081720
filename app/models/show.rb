72df0ee54a9a86d71eb4982351792fc6360928ad
taylorsieling <taylorasieling@gmail.com>
2020-10-29 21:10:20 -0500

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
   
+    has_many :characters
+    has_many :shows, through: :characters
+    belongs_to :network
+
+    def actors_list
+        self.characters.map do |a|
+            a.actor.full_name
+        end
+    end
+
 end
